dfc2247b048b2857a6fe69d72f9233c68b004b63
Zinki06 <76738954+Zinki06@users.noreply.github.com>
2024-06-14 11:12:57 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
 
-# 스크랩할 웹페이지 URL
+
 url = "https://guahm.sen.hs.kr/18515/subMenu.do"
 
 # 페이지의 HTML 콘텐츠 가져오기
 response = requests.get(url)
 response.raise_for_status()  # 요청이 성공했는지 확인
 
-# BeautifulSoup를 사용해 HTML 콘텐츠 파싱
+#HTML 콘텐츠 파싱
 soup = BeautifulSoup(response.content, 'html.parser')
 
 # 급식 일정과 날짜 추출
@@ -28,6 +29,11 @@ if table:
             dates.append(date)
             meal_schedule.append(meal)
 
-# 추출된 데이터 출력
-for date, meal in zip(dates, meal_schedule):
-    print(f"날짜: {date}, 식단: {meal}")
+# 데이터 CSV 파일로 저장
+with open('meal_schedule.csv', 'w', newline='', encoding='utf-8') as file:
+    writer = csv.writer(file)
+    writer.writerow(['날짜', '식단'])
+    for date, meal in zip(dates, meal_schedule):
+        writer.writerow([date, meal])
+
+print("급식 일정 'meal_schedule.csv' 저장 완료")
